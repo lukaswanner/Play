7c53845e07f99631eeb6ac1981ef50f104decb94
Lukas <lukas.wanner@web.de>
2020-12-10 11:52:15 +0100

    started with vue

--- public/javascripts/vue.js
+++ public/javascripts/vue.js
@@ -1,16 +1,31 @@
-var test =  new Vue({
-    el: "#test",
-    data: {
-        message: "Hello World"
-    },
-    methods: {
-        submit: function () {
-            this.message = "yeah"
+let currHand = [{text: "None", Points: "0"}]
+for (let index = 1; index < 9; index++) {
+    currHand.push({text: index, Points: "Points: " + index})
+}
+
+
+Vue.component('myHand', {
+    template: `
+        <div class="myHand">
+            <div v-for="card in hand" class="inHand">
+                {{card.text}}
+                {{card.Points}}
+            </div>  
+        </div>
+    `,
+    data: function () {
+        return {
+            hand: currHand
         }
-    }
+    },
+
 })
 
-
-let cards = new Vue({
-    el: ".myHand"
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
 })
